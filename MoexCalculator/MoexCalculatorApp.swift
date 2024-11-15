3d8ab6849f198f568278786439a7198bf3dd4b99
DGalbacheva <dglbacheva@gmail.com>
2024-11-15 15:32:09 +0300

    Created work space for MoexCalculator app

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by MacBook Pro 15 on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
