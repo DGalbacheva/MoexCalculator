c4b6e387581ac99974e433bfc3b443d0459d6ded
DGalbacheva <dglbacheva@gmail.com>
2024-11-15 17:02:56 +0300

    In CalculatorModel added: A directory in which we will store exchange rates; setCurrencyRates function, it sets the values ​​of rates; The logic of the model is the function of converting a currency amount into another currency.

--- MoexCalculator/CalculatorModel.swift
+++ MoexCalculator/CalculatorModel.swift
@@ -2,7 +2,29 @@
 //  CalculatorModel.swift
 //  MoexCalculator
 //
-//  Created by MacBook Pro 15 on 15.11.2024.
+//  Created by Doroteya Galbacheva on 15.11.2024.
 //
 
 import Foundation
+
+struct CalculatorModel {
+
+    private(set) var currencyRates: CurrencyRates = [.RUR: 1, .CNY: 12]
+ 
+    mutating func setCurrencyRates(_ currencyRates: CurrencyRates) {
+        self.currencyRates = currencyRates
+    }
+    
+    func convert(_ source: CurrencyAmount, to target: Currency) -> Double {
+        guard
+            let sourceRate = currencyRates[source.currency],
+            let targetRate = currencyRates[target]
+        else { return 0 }
+        
+        if targetRate.isZero {
+            return 0
+        } else {
+            return source.amount * sourceRate / targetRate
+        }
+    }
+}
