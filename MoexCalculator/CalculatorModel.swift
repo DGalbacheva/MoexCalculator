41d049c03b6e0f84f8278170f734403506a76e26
DGalbacheva <dglbacheva@gmail.com>
2024-11-15 17:15:49 +0300

    The ViewModel notifies state changes using the ObservableObject protocol and variables with the @Published attribute

--- MoexCalculator/CalculatorModel.swift
+++ MoexCalculator/CalculatorModel.swift
@@ -9,12 +9,15 @@ import Foundation
 
 struct CalculatorModel {
 
+    //1
     private(set) var currencyRates: CurrencyRates = [.RUR: 1, .CNY: 12]
  
+    //2
     mutating func setCurrencyRates(_ currencyRates: CurrencyRates) {
         self.currencyRates = currencyRates
     }
     
+    //3
     func convert(_ source: CurrencyAmount, to target: Currency) -> Double {
         guard
             let sourceRate = currencyRates[source.currency],
