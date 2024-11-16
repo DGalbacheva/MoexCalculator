241cc8eef19468ca87ec4e8e6ef36c40682d35d4
DGalbacheva <dglbacheva@gmail.com>
2024-11-16 21:20:41 +0300

    Improved the code in the file CalculatorView.swift — fragments with duplicate code.

---
+++ MoexCalculator/CurrencyInput.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyInput.swift
+//  MoexCalculator
+//
+//  Created by MacBook Pro 15 on 16.11.2024.
+//
+
+import SwiftUI
+
+struct CurrencyInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyInput()
+}
