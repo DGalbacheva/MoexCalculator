45777cadc62fdf713e30737301151e8f64e25296
DGalbacheva <dglbacheva@gmail.com>
2024-11-16 20:30:50 +0300

    ContentView deleted and created CalculatorView

---
+++ MoexCalculator/CalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorView.swift
+//  MoexCalculator
+//
+//  Created by MacBook Pro 15 on 16.11.2024.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
