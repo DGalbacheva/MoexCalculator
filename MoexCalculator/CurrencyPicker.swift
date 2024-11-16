426dae6e498170882544e1afbdfc9a5cf93c2dd4
DGalbacheva <dglbacheva@gmail.com>
2024-11-16 21:27:37 +0300

    Created components that help select currencies from a list.

---
+++ MoexCalculator/CurrencyPicker.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyPicker.swift
+//  MoexCalculator
+//
+//  Created by MacBook Pro 15 on 16.11.2024.
+//
+
+import SwiftUI
+
+struct CurrencyPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyPicker()
+}
